095777ecb871c50f7c8d7b9f52f4034e3836482e
Farid Sakhyzad <buterbread@gmail.com>
2018-03-23 23:04:52 +0200

    WIP

---
+++ www/build/0._build.js
@@ -0,0 +1,23 @@
+webpackJsonp([0],{
+
+/***/ 42:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__components_HelloScreen_vue__ = __webpack_require__(10);
+
+
+/* harmony default export */ __webpack_exports__["default"] = (function () {
+	return {
+		name: 'hello-screen',
+
+		render: function render(h) {
+			return h(__WEBPACK_IMPORTED_MODULE_0__components_HelloScreen_vue__["a" /* default */], { props: { type: 'board' } });
+		}
+	};
+});
+
+/***/ })
+
+});
